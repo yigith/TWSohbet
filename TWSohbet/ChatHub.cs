0fa05ca5f10d23a7cfc0a99de306fabbdd5e559a
Yigit Haciefendioglu <yigith1@gmail.com>
2020-12-23 22:03:34 +0300

    Add project files.

---
+++ TWSohbet/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TWSohbet
+{
+    public class ChatHub : Hub
+    {
+        // client'lardan gelen mesajı karşılar
+        public void SendMessage(string username, string message)
+        {
+            Clients.All.newMessage(username, message);
+        }
+    }
+}
---
+++ TWSohbet/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(TWSohbet.Startup))]
+
+namespace TWSohbet
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            // https://docs.microsoft.com/en-us/aspnet/signalr/overview/getting-started/tutorial-getting-started-with-signalr-and-mvc
+            app.MapSignalR();
+        }
+    }
+}
